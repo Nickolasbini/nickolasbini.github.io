d908c6ce4db9b91a65f9a9eaff58cf50248ca353
Nickolas Bini <nickolasbini@hotmail.com>
2024-10-31 22:24:42 -0300

    Build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
+  publicPath: process.env.VUE_APP_PRODUCTION === 'production' ? '/nickolasbini.github.io/' : '/',
   transpileDependencies: true
 })
